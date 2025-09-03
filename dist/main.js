b24d1e7f573690ed55edcc7b25b45184860966b7
Nour-ibrahem <nouribrahem207@gmail.com>
2025-09-03 22:35:25 +0300

    Complete the project

---
+++ dist/main.js
@@ -0,0 +1,29 @@
+"use strict";
+let checkBoxes = document.querySelectorAll("input[type='checkbox']");
+let products = document.querySelectorAll(".col-lg-3.col-md-6.col-sm-12");
+checkBoxes.forEach((checkbox) => {
+    checkbox.addEventListener("change", () => {
+        let activeFilters = [];
+        checkBoxes.forEach(cb => {
+            if (cb.checked) {
+                activeFilters.push(cb.dataset.product || "");
+            }
+        });
+        products.forEach(product => {
+            let productType = product.dataset.product;
+            if (activeFilters.length === 0) {
+                product.classList.remove("hide");
+                product.classList.add("show");
+            }
+            else if (productType && activeFilters.includes(productType)) {
+                product.classList.remove("hide");
+                product.classList.add("show");
+            }
+            else {
+                product.classList.remove("show");
+                product.classList.add("hide");
+            }
+        });
+    });
+});
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,32 @@
+// Select Elements
+let checkBoxes = document.querySelectorAll<HTMLInputElement>("input[type='checkbox']");
+let products = document.querySelectorAll<HTMLElement>(".col-lg-3.col-md-6.col-sm-12");
+
+// Handle Change Event
+checkBoxes.forEach((checkbox) => {
+    checkbox.addEventListener("change", () => {
+        // Collect all checked values
+        let activeFilters: string[] = [];
+        checkBoxes.forEach(cb => {
+            if (cb.checked) {
+                activeFilters.push(cb.dataset.product || "");
+            }
+        });
+
+        products.forEach(product => {
+            let productType = product.dataset.product;
+
+            if (activeFilters.length === 0) {
+
+                product.classList.remove("hide");
+                product.classList.add("show");
+            } else if (productType && activeFilters.includes(productType)) {
+                product.classList.remove("hide");
+                product.classList.add("show");
+            } else {
+                product.classList.remove("show");
+                product.classList.add("hide");
+            }
+        });
+    });
+});
